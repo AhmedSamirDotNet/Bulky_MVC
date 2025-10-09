cea64dec4ea2171431ab8f7d0f25a4900992d368
AhmedSamirDotNet <ahmedsamirr912@gmil.com>
2025-10-09 22:13:40 +0300

    HomeView

--- BulkeyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkeyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Bulky.DataAccess.Models;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkeyWeb.Areas.Customer.Controllers
@@ -8,15 +10,27 @@ namespace BulkeyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _UOF;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger , IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _UOF = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> objProductList = _UOF.Products.GetAll(includeProperties: "Category");
+
+            return View(objProductList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product objProduct = _UOF.Products.Get(p=>(p.Id==id),includeProperties: "Category");
+
+            return View(objProduct);
         }
 
         public IActionResult Privacy()
