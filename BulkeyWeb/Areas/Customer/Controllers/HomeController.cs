9211a880f3a454ae9c25067e68dfc844f3dc5990
AhmedSamirDotNet <ahmedsamirr912@gmil.com>
2025-10-10 15:50:57 +0300

    Revert "HomeView"
    This reverts commit cea64dec4ea2171431ab8f7d0f25a4900992d368.

--- BulkeyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkeyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 using System.Diagnostics;
 using Bulky.DataAccess.Models;
-using Bulky.DataAccess.Repository.IRepository;
-using Bulky.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkeyWeb.Areas.Customer.Controllers
@@ -10,27 +8,15 @@ namespace BulkeyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _UOF;
 
-
-        public HomeController(ILogger<HomeController> logger , IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _UOF = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Product> objProductList = _UOF.Products.GetAll(includeProperties: "Category");
-
-            return View(objProductList);
-        }
-
-        public IActionResult Details(int id)
-        {
-            Product objProduct = _UOF.Products.Get(p=>(p.Id==id),includeProperties: "Category");
-
-            return View(objProduct);
+            return View();
         }
 
         public IActionResult Privacy()
